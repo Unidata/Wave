ad0cdd4738729457ee304760e9efc56008ceeacc
Joshua Clark <jclark754@gmail.com>
2015-06-27 01:22:19 -0600

    cleaner css implementation

--- jc_dump/mainwindow.py
+++ jc_dump/mainwindow.py
@@ -1,8 +1,13 @@
 import sys
 from PyQt4 import QtGui, Qt, QtCore
-from jc_dump.skewT_dialog import skewt_dialog
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from jc_dump.skewT_dialog import SkewTDialog
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
+import numpy as np
+from metpy.calc import get_wind_components, lcl, dry_lapse, parcel_profile
+from metpy.plots import SkewT
+from metpy.units import units, concatenate
 
 
 class Window(QtGui.QMainWindow):
@@ -15,60 +20,30 @@ class Window(QtGui.QMainWindow):
         screen = QtGui.QDesktopWidget().screenGeometry()
         self.setGeometry(0, 0, screen.width(), screen.height())
 
-        css = """
-                QWidget{
-                    Background: #4c4c4c;
-                    color:white;
-                    font:14px bold;
-                    font-weight:bold;
-                    border-radius: 1px;
-                }
-
-                QMenuBar::item {
-                    spacing: 3px; /* spacing between menu bar items */
-                    padding: 10px 4px 4px 4px;
-                    background: transparent;
-                    font:16px bold;
-                }
-
-                QMenuBar::item:selected { /* when selected using mouse or keyboard */
-                    background: #666666;
-                }
-
-                QMenu::item:selected {
-                    background: #666666;
-                }
-
-                QToolButton{
-                    Background: transparent;
-                }
-                QToolButton:hover{
-                    Background: #666666;
-                }
-                """
-
-        self.setStyleSheet(css)
+        stylesheet = "css/MainWindow.css"
+        with open(stylesheet, "r") as ssh:
+            self.setStyleSheet(ssh.read())
         self.setAutoFillBackground(True)
         self.setBackgroundRole(QtGui.QPalette.Highlight)
 
-        exitaction = QtGui.QAction(QtGui.QIcon('./img/exit_64px.png'), 'Exit', self)
-        exitaction.setShortcut('Ctrl+Q')
-        exitaction.setStatusTip('Exit application')
-        exitaction.triggered.connect(self.close)
-        skewtaction = QtGui.QAction(QtGui.QIcon('./img/skewt_64px.png'), 'Skew-T', self)
-        skewtaction.setShortcut('Ctrl+S')
-        skewtaction.setStatusTip('Open Skew-T Dialog Box')
-        skewtaction.triggered.connect(self.skewt_dialog)
+        exit_action = QtGui.QAction(QtGui.QIcon('./img/exit_64px.png'), 'Exit', self)
+        exit_action.setShortcut('Ctrl+Q')
+        exit_action.setStatusTip('Exit application')
+        exit_action.triggered.connect(self.close)
+        skewt_action = QtGui.QAction(QtGui.QIcon('./img/skewt_64px.png'), 'Skew-T', self)
+        skewt_action.setShortcut('Ctrl+S')
+        skewt_action.setStatusTip('Open Skew-T Dialog Box')
+        skewt_action.triggered.connect(self.skewt_dialog)
 
         menubar = self.menuBar()
         menubar.setNativeMenuBar(False)
         filemenu = menubar.addMenu('&File')
         editmenu = menubar.addMenu('&Edit')
-        filemenu.addAction(exitaction)
+        filemenu.addAction(exit_action)
 
         toolbar = self.addToolBar('Exit')
-        toolbar.addAction(exitaction)
-        toolbar.addAction(skewtaction)
+        toolbar.addAction(exit_action)
+        toolbar.addAction(skewt_action)
 
         self.statusBar().showMessage('Ready')
         self.setWindowTitle("WAVE (Skew-T Viewer)")
@@ -82,8 +57,58 @@ class Window(QtGui.QMainWindow):
         self.show()
 
     def skewt_dialog(self):
-        self.dialog = skewt_dialog()
+        self.dialog = SkewTDialog()
         self.dialog.exec_()
+
+    def plot(self):
+
+        p, T, Td, direc, spd = np.loadtxt(open('may3_sounding.txt'),
+                usecols=(0, 2, 3, 6, 7), unpack=True)
+        p = p * units.mbar
+        T = T * units.degC
+        Td = Td * units.degC
+        spd = spd * units.knot
+        direc = direc * units.deg
+
+        u, v = get_wind_components(spd, direc)
+        # Create a new figure. The dimensions here give a good aspect ratio
+        skew = SkewT(self.figure, rotation=45)
+
+        # Plot the data using normal plotting functions, in this case using
+        # log scaling in Y, as dictated by the typical meteorological plot
+        skew.plot(p, T, 'r')
+        skew.plot(p, Td, 'g')
+        skew.plot_barbs(p, u, v)
+        skew.ax.set_ylim(1000, 100)
+        skew.ax.set_xlim(-40, 60)
+
+        # Calculate LCL height and plot as black dot
+        l = lcl(p[0], T[0], Td[0])
+        lcl_temp = dry_lapse(concatenate((p[0], l)), T[0])[-1].to('degC')
+        skew.plot(l, lcl_temp, 'ko', markerfacecolor='black')
+
+        # Calculate full parcel profile and add to plot as black line
+        prof = parcel_profile(p, T[0], Td[0]).to('degC')
+        skew.plot(p, prof, 'k', linewidth=2)
+
+        # Example of coloring area between profiles
+        skew.ax.fill_betweenx(p, T, prof, where=T>=prof, facecolor='blue', alpha=0.4)
+        skew.ax.fill_betweenx(p, T, prof, where=T<prof, facecolor='red', alpha=0.4)
+
+        # An example of a slanted line at constant T -- in this case the 0
+        # isotherm
+        l = skew.ax.axvline(0, color='c', linestyle='--', linewidth=2)
+
+        # Add the relevant special lines
+        skew.plot_dry_adiabats()
+        skew.plot_moist_adiabats()
+        skew.plot_mixing_lines()
+
+        # Discards old graph, works poorly though
+        #skew.ax.hold(False)
+
+        # refresh canvas
+        self.canvas.draw()
 
 
 def main():
--- jc_dump/skewT_dialog.py
+++ jc_dump/skewT_dialog.py
@@ -1,48 +1,19 @@
 from PyQt4 import QtGui
-#from siphon.catalog import TDSCatalog
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
-import matplotlib.pyplot as plt
-import numpy as np
-from metpy.calc import get_wind_components, lcl, dry_lapse, parcel_profile
-from metpy.plots import SkewT
-from metpy.units import units, concatenate
+# from siphon.catalog import TDSCatalog
 
 
-class skewt_dialog(QtGui.QDialog):
+class SkewTDialog(QtGui.QDialog):
     def __init__(self, parent=None):
-        super(skewt_dialog, self).__init__()
+        super(SkewTDialog, self).__init__()
+
         self.setWindowTitle("Skew-T Menu")
         self.setGeometry(0, 0, 450, 300)
-        self.move(QtGui.QApplication.desktop().screen().rect().center()- self.rect().center())
-        css = """
-                QWidget{
-                    Background: #4c4c4c;
-                    color:white;
-                    font:14px bold;
-                    font-weight:bold;
-                    border-radius: 1px;
-                }
+        self.move(QtGui.QApplication.desktop().screen().rect().center() - self.rect().center())
 
-                QLineEdit{
-                    Background: #666666;
-                    padding: 4px 4px 4px 4px;
-                }
 
-                QComboBox{
-                    Background: #666666;
-                    padding: 4px 4px 4px 4px;
-                }
-
-                QPushButton{
-                    Background: #999999;
-                    padding: 4px 4px 4px 4px;
-                    border-radius: 5px;
-
-                }
-                """
-
-        self.setStyleSheet(css)
+        stylesheet = "css/SkewT.css"
+        with open(stylesheet, "r") as ssh:
+            self.setStyleSheet(ssh.read())
         self.setAutoFillBackground(True)
         self.setBackgroundRole(QtGui.QPalette.Highlight)
 
@@ -50,11 +21,11 @@ class skewt_dialog(QtGui.QDialog):
         grid.setSpacing(10)
         self.setLayout(grid)
 
-
         self.lbl = QtGui.QLabel("Data Source:", self)
         grid.addWidget(self.lbl, 0, 0, 1, 1)
         combo = QtGui.QComboBox(self)
         combo.addItem("NCSS")
+        combo.activated[str].connect(self.combo_data)
         grid.addWidget(combo, 0, 1, 1, 1)
 
         self.lbl = QtGui.QLabel("Latitude:", self)
@@ -67,63 +38,14 @@ class skewt_dialog(QtGui.QDialog):
         longbox = QtGui.QLineEdit(self)
         grid.addWidget(longbox, 2, 1, 1, 1)
 
-        #combo.activated[str].connect(self.onActivated)
         submit_button = QtGui.QPushButton('Plot')
-        submit_button.clicked.connect(self.plot)
-
+        submit_button.clicked.connect(self.window_data)
         grid.addWidget(submit_button, 3, 0, 1, 3)
 
         self.show()
 
-    def get_data(self):
+    def combo_data(self, text):
+        return text
+
+    def window_data(self):
         pass
-
-    def plot(self):
-
-        p, T, Td, direc, spd = np.loadtxt(open('may3_sounding.txt'),
-                usecols=(0, 2, 3, 6, 7), unpack=True)
-        p = p * units.mbar
-        T = T * units.degC
-        Td = Td * units.degC
-        spd = spd * units.knot
-        direc = direc * units.deg
-
-        u, v = get_wind_components(spd, direc)
-        # Create a new figure. The dimensions here give a good aspect ratio
-        skew = SkewT(self.figure, rotation=45)
-
-        # Plot the data using normal plotting functions, in this case using
-        # log scaling in Y, as dictated by the typical meteorological plot
-        skew.plot(p, T, 'r')
-        skew.plot(p, Td, 'g')
-        skew.plot_barbs(p, u, v)
-        skew.ax.set_ylim(1000, 100)
-        skew.ax.set_xlim(-40, 60)
-
-        # Calculate LCL height and plot as black dot
-        l = lcl(p[0], T[0], Td[0])
-        lcl_temp = dry_lapse(concatenate((p[0], l)), T[0])[-1].to('degC')
-        skew.plot(l, lcl_temp, 'ko', markerfacecolor='black')
-
-        # Calculate full parcel profile and add to plot as black line
-        prof = parcel_profile(p, T[0], Td[0]).to('degC')
-        skew.plot(p, prof, 'k', linewidth=2)
-
-        # Example of coloring area between profiles
-        skew.ax.fill_betweenx(p, T, prof, where=T>=prof, facecolor='blue', alpha=0.4)
-        skew.ax.fill_betweenx(p, T, prof, where=T<prof, facecolor='red', alpha=0.4)
-
-        # An example of a slanted line at constant T -- in this case the 0
-        # isotherm
-        l = skew.ax.axvline(0, color='c', linestyle='--', linewidth=2)
-
-        # Add the relevant special lines
-        skew.plot_dry_adiabats()
-        skew.plot_moist_adiabats()
-        skew.plot_mixing_lines()
-
-        # Discards old graph, works poorly though
-        #skew.ax.hold(False)
-
-        # refresh canvas
-        self.canvas.draw()
